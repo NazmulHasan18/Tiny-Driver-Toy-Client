02817ba6c70673554643e3adfb6edd73c171c785
NazmulHasan18 <nazmul182218@gmail.com>
2023-05-19 00:24:28 +0600

    Car Toy Gallery added

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "react-toastify/dist/ReactToastify.css";
 import "./index.css";
+import "swiper/css";
 import { RouterProvider } from "react-router-dom";
 import router from "./routes/Route";
 import { ToastContainer } from "react-toastify";
---
+++ src/pages/home/Gallery/Gallery.jsx
@@ -0,0 +1,82 @@
+import { useEffect, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import "swiper/css/effect-coverflow";
+import { Navigation, Pagination, EffectCoverflow, Autoplay } from "swiper";
+
+const Gallery = () => {
+   const [cars, setCars] = useState([]);
+   useEffect(() => {
+      fetch("http://localhost:4000/car_gallery")
+         .then((res) => res.json())
+         .then((data) => setCars(data));
+   }, []);
+
+   return (
+      <div className="my-24">
+         <div className="text-center text-white mb-10 space-y-3">
+            <h2 className="text-4xl font-bold ">Car Toy Gallery</h2>
+            <p className="text-xl font-semibold">Unleash the Joy of Miniature Racing</p>
+         </div>
+         <Swiper
+            effect={"coverflow"}
+            grabCursor={true}
+            centeredSlides={true}
+            slidesPerView={1}
+            spaceBetween={30}
+            autoplay={{
+               delay: 3000,
+               disableOnInteraction: false,
+            }}
+            loop={true}
+            pagination={{
+               clickable: true,
+            }}
+            breakpoints={{
+               640: {
+                  slidesPerView: 1,
+                  spaceBetween: 20,
+               },
+               768: {
+                  slidesPerView: 3,
+                  spaceBetween: 40,
+               },
+            }}
+            coverflowEffect={{
+               rotate: 40,
+               stretch: 0,
+               depth: 100,
+               modifier: 1,
+               slideShadows: true,
+            }}
+            navigation={true}
+            modules={[Pagination, Navigation, EffectCoverflow, Autoplay]}
+            className="mySwiper"
+         >
+            {cars.map((car) => (
+               <SwiperSlide key={car._id}>
+                  <div className="card w-80 md:w-96 mx-auto bg-base-100 shadow-xl">
+                     <figure>
+                        <img src={car.image} alt="" className=" h-60 rounded-lg" />
+                     </figure>
+                     <div className="card-body text-center">
+                        <h2 className="card-title mx-auto font-bold">{car.name}</h2>
+                        <div className="flex justify-center items-center">
+                           <p className="font-bold">
+                              Price: <span className="text-green-500">${car.price}</span>
+                           </p>
+                           <p className="font-bold">
+                              Ratings: <span className="text-yellow-500">{car.rating}</span>
+                           </p>
+                        </div>
+                     </div>
+                  </div>
+               </SwiperSlide>
+            ))}
+         </Swiper>
+      </div>
+   );
+};
+
+export default Gallery;
